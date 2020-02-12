f8789e2d96836d0649779c4ba8c43b4f39ab6f0b
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-02-12 17:32:28 +0100

    intro done

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,53 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        Tim Huijkman Portfolio by Tim Huijkman
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div>
+            <h1 class="title">
+                Tim Huijkman
+            </h1>
+            <h2 class="subtitle">
+                Front-end Developer, Wanna be designer
+            </h2>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+    // import circel from '~/components/circel.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
+    export default {
+        components: {
+            // circel
+        }
+    }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+    @import '~/styles/colors.scss';
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    .container {
+        margin: 0 auto;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+    .title {
+        font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+        display: block;
+        font-weight: 300;
+        font-size: 100px;
+        color: $primary;
+        letter-spacing: 1px;
+    }
 
-.links {
-  padding-top: 15px;
-}
+    .subtitle {
+        font-weight: 300;
+        font-size: 42px;
+        color: $alternative;
+        word-spacing: 5px;
+        padding-bottom: 15px;
+    }
 </style>
